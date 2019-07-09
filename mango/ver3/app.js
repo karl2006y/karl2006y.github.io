3bd3cb5ec2b5576a24c293c3898951a89195b9c3
karl2006y <karl2006y@gmail.com>
2019-07-09 17:07:51 +0800

    Add files via upload

--- mango/ver3/app.js
+++ mango/ver3/app.js
@@ -19,31 +19,31 @@ const vm = new Vue({
   created: function() {
     let self = this;
     self.liff =  window.liff;
-    console.log(self.liff)
+    console.log(self.liff);
   },
   watch: {
-    show: function() {
-      let self = this;
-      if (self.show == "historyList") {
-        setTimeout(function() {
-          self.getList();
-        }, 2000);
-      }
-    }
+    // show: function() {
+    //   let self = this;
+    //   if (self.show == "historyList") {
+    //     setTimeout(function() {
+    //       self.getList();
+    //     }, 2000);
+    //   }
+    // }
   },
   methods: {
-    getList: function() {
-      let self = this;
-      self.loading = true;
-      axios({
-        methods: "get",
-        url:
-          "https://script.google.com/macros/s/AKfycbz-7KYcM8ZYDsGIQcb_TLyZTyUdTYyunSUnYOEPxA/exec?method=getList&recphone=" +
-          self.registerData.userData.phone
-      }).then(resp => {
-        self.api = resp.data;
-        self.loading = false;
-      });
-    }
+    // getList: function() {
+    //   let self = this;
+    //   self.loading = true;
+    //   axios({
+    //     methods: "get",
+    //     url:
+    //       "https://script.google.com/macros/s/AKfycbz-7KYcM8ZYDsGIQcb_TLyZTyUdTYyunSUnYOEPxA/exec?method=getList&recphone=" +
+    //       self.registerData.userData.phone
+    //   }).then(resp => {
+    //     self.api = resp.data;
+    //     self.loading = false;
+    //   });
+    // }
   }
 });
